c042ccda164328772d96a3ea2774f60c645df06f
Pol <Tihersarc35@gmail.com>
2024-01-19 20:21:45 +0100

    Update

--- moviecatalog/app/src/main/java/com/example/movie_catalog/MainActivity.kt
+++ moviecatalog/app/src/main/java/com/example/movie_catalog/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.movie_catalog
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,12 +11,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.example.movie_catalog.ui.theme.MoviecatalogTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Ejemplo de manejo de errores
+        lifecycleScope.launch {
+            try {
+                val topRatedMovies = MovieApi.retrofitService.getTopRatedMovies(API_KEY)
+                Log.d("MainActivity", "Fetching top-rated movies...")
+            } catch (e: Exception) {
+                // Manejo de errores: Muestra un mensaje de error en la interfaz de usuario
+                e.printStackTrace()
+                Log.e("MainActivity", "Error fetching top-rated movies", e)
 
+            }
+        }
     }
 }
